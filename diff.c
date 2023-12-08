f0cee8a42bb479347dbb846087ac08fae8e51d34
hribz <1918256943@qq.com>
2023-12-08 11:11:24 +0800

    test git diff

---
+++ diff.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <immintrin.h>
+
+// test.c
+/* this is 
+a test file */
+#define offset 1
+#define func(a) int aa;\
+                a=100;\
+int a=0;
+
+#ifdef __x86_64__
+#define x86_defined 1
+#elif defined(__arm__)
+#define arm_defined 2
+#elif defined(__aarch64__)
+#define aarch_defined 3
+#endif
+
+int main() {
+    #if (x86_defined && a) && defined(__x86_64__)
+    printf("x86\n");
+    #endif
+
+    #if arm_defined
+    printf("arm\n");
+    #endif
+    return 0;
+}
